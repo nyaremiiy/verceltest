86c2a683e810f3e6c6541800e4ec36ddc8f21f11
Nazar Yaremii <n.yaremiy791@gmail.com>
2023-03-10 11:37:17 +0200

    3

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-alert('Working');
-console.log('Working');
+// Add Express
+const express = require("express");
+
+// Initialize Express
+const app = express();
+
+// Create GET request
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
+
+// Initialize server
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
